d0d1b4a3e6c658e9f58e15c32de49c7f49ea4f63
PlayWithDarik <49361988+PlayWithDarik@users.noreply.github.com>
2021-10-06 18:07:50 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -80,7 +80,7 @@ elif [[ $de_setting == 4 ]]; then
 elif [[ $de_setting == 5 ]]; then
   pacman -S plasma kde-applications --noconfirm
 elif [[ $de_setting == 6 ]]; then
-  pacman -S plasma-desktop kde-applications --noconfirm
+  pacman -S plasma konsole --noconfirm
 fi
 
 echo 'Выбираем DM'
