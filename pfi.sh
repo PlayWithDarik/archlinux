e28f8a614e0d8e7ffb48b02ac2ba7015caf20621
PlayWithDarik <49361988+PlayWithDarik@users.noreply.github.com>
2021-11-06 15:11:46 +0200

    Update pfi.sh

--- pfi.sh
+++ pfi.sh
@@ -57,18 +57,9 @@ swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 
 echo 'Выбор зеркал для загрузки.'
-read -p "1 - archlinux.ip-connect.vn.ua, 2 - mirohost.net, 3 - nix.org.ua, 4 - astra.in.ua, 5 - endpoint.ml:" mirrors_setting
-if   [[ $mirrors_setting == 1 ]]; then
-  echo "Server = http://archlinux.ip-connect.vn.ua/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-elif [[ $mirrors_setting == 2 ]]; then
-  echo "Server = http://mirror.mirohost.net/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-elif [[ $mirrors_setting == 3 ]]; then
-  echo "Server = http://mirrors.nix.org.ua/linux/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-elif [[ $mirrors_setting == 4 ]]; then
-  echo "Server = http://archlinux.astra.in.ua/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-elif [[ $mirrors_setting == 5 ]]; then
-  echo "Server = http://repo.endpoint.ml/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-fi
+rm -rf /etc/pacman.d/mirrorlist
+wget https://git.io/mirrorlist
+mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
 
 echo 'Установка основных пакетов'
 pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware nano dhcpcd netctl e2fsprogs
