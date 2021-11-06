a2caf373b8257bb2545dd13869e8f4373b66439d
PlayWithDarik <49361988+PlayWithDarik@users.noreply.github.com>
2021-11-06 21:50:56 +0200

    Update pfi.sh

--- pfi.sh
+++ pfi.sh
@@ -56,10 +56,28 @@ mount /dev/sda1 /mnt/boot
 swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 
-echo 'Выбор зеркал для загрузки.'
-rm -rf /etc/pacman.d/mirrorlist
-wget https://git.io/mirrorlist
-mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
+echo 'Зеркала для загрузки.'
+> /etc/pacman.d/mirrorlist
+cat <<EOF >>/etc/pacman.d/mirrorlist
+
+##
+## Arch Linux repository mirrorlist
+## Generated on 2021-11-06
+##
+
+## Ukraine
+Server = https://archlinux.astra.in.ua/$repo/os/$arch
+Server = https://repo.endpoint.ml/archlinux/$repo/os/$arch
+Server = https://archlinux.ip-connect.vn.ua/$repo/os/$arch
+Server = https://mirror.mirohost.net/archlinux/$repo/os/$arch
+Server = https://mirrors.nix.org.ua/linux/archlinux/$repo/os/$arch
+
+EOF
+
+echo 'Активируем новые зеркала'
+pacman-key --init
+pacman-key --populate archlinux
+pacman -Sy
 
 echo 'Установка основных пакетов'
 pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware nano dhcpcd netctl e2fsprogs
