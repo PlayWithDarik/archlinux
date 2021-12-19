d7ae8513c238bf50bbcb4520f90919e61681f2fe
PlayWithDarik <49361988+PlayWithDarik@users.noreply.github.com>
2021-12-19 19:08:46 +0200

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo -e "\nINSTALLING AUR SOFTWARE\n"
+# You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
 
 echo "CLONING: YAY"
 cd ~
 git clone "https://aur.archlinux.org/yay.git"
 cd ${HOME}/yay
 makepkg -si --noconfirm
+
+PKGS=(
+'google-chrome' # Google Chrome
+)
+
+
+for PKG in "${PKGS[@]}"; do
+    yay -S --noconfirm $PKG
+done
+
+echo -e "\nDone!\n"
