31aa11da2ba6271efafa5b6c4f2ff66026b614b5
PlayWithDarik <49361988+PlayWithDarik@users.noreply.github.com>
2022-12-17 14:45:27 +0200

    Update pfi.sh

--- pfi.sh
+++ pfi.sh
@@ -11,53 +11,56 @@ timedatectl set-ntp true
 echo 'Форматирования диска'
 wipefs --all /dev/sda
 
+echo 'BTRFS'
+pacman -S btrfs-progs
+
 echo 'Создание разделов'
 (
-  echo o;
+ echo g;
 
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +512M;
+ echo n;
+ echo ;
+ echo;
+ echo +300M;
+ echo y;
+ echo t;
+ echo 1;
 
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +30G;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +4096M;
-
-  echo n;
-  echo p;
-  echo;
-  echo;
-  echo a;
-  echo 1;
-
-  echo w;
+ echo n;
+ echo;
+ echo;
+ echo +30G;
+ echo y;
+ 
+  
+ echo n;
+ echo;
+ echo;
+ echo;
+ echo y;
+  
+ echo w;
 ) | fdisk /dev/sda
 
 echo 'Ваша разметка диска'
 fdisk -l
 
 echo 'Форматирование дисков'
-mkfs.ext2  /dev/sda1 -L boot
-mkfs.ext4  /dev/sda2 -L root
-mkswap /dev/sda3 -L swap
-mkfs.ext4  /dev/sda4 -L home
+mkfs.ext2 /dev/sda1
+mkswap /dev/sda2 
+mkfs.btrfs /dev/sda3
 
 echo 'Монтирование дисков'
-mount /dev/sda2 /mnt
-mkdir /mnt/{boot,home}
-mount /dev/sda1 /mnt/boot
+mount /dev/sda3 /mnt
+cd /mnt
+btrfs subvolume create ./@
+btrfs subvolume create ./@home
 swapon /dev/sda3
-mount /dev/sda4 /mnt/home
+cd
+umount /mnt -R
+mount -o rw,noatime,compress=zstd:3,ssd,ssd_spread,discard=async,space_cache=v2,subvol=/@ dev/sda3 /mnt
+mkdir /mnt/home
+mount -o rw,noatime,compress=zstd:3,ssd,ssd_spread,discard=async,space_cache=v2,subvol=/@home dev/sda3 /mnt/home
 
 echo 'Зеркала для загрузки.'
 cat > /etc/pacman.d/mirrorlist <<EOF
@@ -76,7 +79,7 @@ Server = https://mirrors.nix.org.ua/linux/archlinux/\$repo/os/\$arch
 EOF
 
 echo 'Установка основных пакетов'
-pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware nano dhcpcd netctl dbus e2fsprogs
+pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware nano dhcpcd netctl dbus
 
 echo 'Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
